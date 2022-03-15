a1276250adebe6c0c6d08ff8d6f5eb758532f088
Nathan Turinski <naturins@microsoft.com>
2022-03-15 11:02:38 -0700

    Add a environment var step in container app creation

---
+++ src/utils/workspaceUtils.ts
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { basename } from 'path';
+import { OpenDialogOptions, Uri, workspace } from "vscode";
+import { IActionContext, IAzureQuickPickItem } from "vscode-azureextensionui";
+import { localize } from "./localize";
+
+export async function selectWorkspaceFile(context: IActionContext, placeHolder: string, options: OpenDialogOptions, fileExtension?: string): Promise<string> {
+    let input: IAzureQuickPickItem<string | undefined> | undefined;
+    let quickPicks: IAzureQuickPickItem<string | undefined>[] = [];
+    if (workspace.workspaceFolders?.length === 1) {
+        // if there's a fileExtension, then only populate the quickPick menu with that, otherwise show the current folders in the workspace
+        const files = fileExtension ? await workspace.findFiles(`**/*.${fileExtension}`) : await workspace.findFiles('**/*')
+        quickPicks = files.map((uri: Uri) => {
+            return {
+                label: basename(uri.fsPath),
+                description: uri.fsPath,
+                data: uri.fsPath
+            };
+        });
+
+        quickPicks.push({ label: `$(file-directory) ${localize('browse', 'Browse...')}`, description: '', data: undefined });
+        input = await context.ui.showQuickPick(quickPicks, { placeHolder });
+    }
+
+    return input && input.data ? input.data : (await context.ui.showOpenDialog(options))[0].fsPath;
+}
