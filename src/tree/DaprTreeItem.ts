b510a4232a37708f326ccb9b8bc5f7f997c72658
Nathan Turinski <naturins@microsoft.com>
2022-04-05 10:33:14 -0700

    Add the dapr tree back in

--- src/tree/DaprTreeItem.ts
+++ src/tree/DaprTreeItem.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Dapr } from "@azure/arm-app";
-import { AzExtTreeItem, TreeItemIconPath } from "@microsoft/vscode-azext-utils";
+import { AzExtParentTreeItem, AzExtTreeItem, GenericTreeItem, IActionContext, TreeItemIconPath } from "@microsoft/vscode-azext-utils";
 import { azResourceContextValue } from "../constants";
 import { localize } from "../utils/localize";
 import { treeUtils } from "../utils/treeUtils";
@@ -12,7 +12,7 @@ import { ContainerAppTreeItem } from "./ContainerAppTreeItem";
 import { IAzureResourceTreeItem } from "./IAzureResourceTreeItem";
 
 // https://github.com/microsoft/vscode-azurecontainerapps/issues/55
-export class DaprTreeItem extends AzExtTreeItem implements IAzureResourceTreeItem {
+export class DaprTreeItem extends AzExtParentTreeItem implements IAzureResourceTreeItem {
     public static contextValue: string = 'dapr';
     public readonly contextValue: string = `${DaprTreeItem.contextValue}|${azResourceContextValue}`;
     public readonly parent: ContainerAppTreeItem;
@@ -24,10 +24,23 @@ export class DaprTreeItem extends AzExtTreeItem implements IAzureResourceTreeIte
         super(parent);
         this.label = localize('dapr', 'Dapr');
         this.data = data || {};
-        // this.description = this.parent.data.template?.dapr ? 'Enabled' : 'Disabled';
+        this.description = this.data.enabled ? 'Enabled' : 'Disabled';
     }
 
     public get iconPath(): TreeItemIconPath {
         return treeUtils.getIconPath('dapr_logo');
+    }
+
+    public async loadMoreChildrenImpl(_clearCache: boolean, _context: IActionContext): Promise<AzExtTreeItem[]> {
+        const children: AzExtTreeItem[] = [];
+        this.data.appId ? children.push(new GenericTreeItem(this, { description: 'app id', label: this.data.appId, contextValue: 'daprAppId' })) : undefined;
+        this.data.appPort ? children.push(new GenericTreeItem(this, { description: 'app port', label: String(this.data.appPort), contextValue: 'daprAppPort' })) : undefined;
+        this.data.appProtocol ? children.push(new GenericTreeItem(this, { description: 'app protocol', label: String(this.data.appProtocol), contextValue: 'daprAppProtocol' })) : undefined;
+
+        return children;
+    }
+
+    public hasMoreChildrenImpl(): boolean {
+        return false;
     }
 }
