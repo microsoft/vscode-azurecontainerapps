0e6b364f60340c30bf41a0a3d44a388c8cc32540
MicroFish91 <mwfisher91@gmail.com>
2022-08-04 13:58:56 -0700

    remove resolver try/catch

--- src/ContainerAppsResolver.ts
+++ src/ContainerAppsResolver.ts
@@ -14,15 +14,10 @@ import { getResourceGroupFromId } from "./utils/azureUtils";
 export class ContainerAppsResolver implements AppResourceResolver {
     public async resolveResource(subContext: ISubscriptionContext, resource: AppResource): Promise<ResolvedAppResourceBase | null> {
         return await callWithTelemetryAndErrorHandling('resolveResource', async (context: IActionContext) => {
-            try {
-                const client: ContainerAppsAPIClient = await createContainerAppsAPIClient([context, subContext]);
-                const rgName: string = getResourceGroupFromId(resource.id);
-                const me: ManagedEnvironment = await client.managedEnvironments.get(rgName, resource.name);
-                return new ResolvedContainerAppsResource(subContext, me);
-            } catch (e) {
-                console.error({ ...context, ...subContext });
-                throw e;
-            }
+            const client: ContainerAppsAPIClient = await createContainerAppsAPIClient([context, subContext]);
+            const rgName: string = getResourceGroupFromId(resource.id);
+            const me: ManagedEnvironment = await client.managedEnvironments.get(rgName, resource.name);
+            return new ResolvedContainerAppsResource(subContext, me);
         }) ?? null;
     }
 
