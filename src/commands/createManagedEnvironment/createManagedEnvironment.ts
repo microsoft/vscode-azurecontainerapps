ee81d9e10f5f51f2e8ae1443fe7235ba020551b8
MicroFish91 <mwfisher91@gmail.com>
2022-08-10 18:29:22 -0700

    update createdManagedEnvironment

--- src/commands/createManagedEnvironment/createManagedEnvironment.ts
+++ src/commands/createManagedEnvironment/createManagedEnvironment.ts
@@ -4,13 +4,15 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { SubscriptionTreeItemBase } from "@microsoft/vscode-azext-azureutils";
-import { AzExtTreeItem, IActionContext } from "@microsoft/vscode-azext-utils";
+import { AzExtParentTreeItem, IActionContext } from "@microsoft/vscode-azext-utils";
 import { ext } from "../../extensionVariables";
 import { SubscriptionTreeItem } from "../../tree/SubscriptionTreeItem";
 import { IManagedEnvironmentContext } from "./IManagedEnvironmentContext";
 
-export async function createManagedEnvironment(context: IActionContext, _node?: AzExtTreeItem): Promise<void> {
-    const subscription: SubscriptionTreeItemBase = await ext.rgApi.appResourceTree.showTreeItemPicker<SubscriptionTreeItemBase>(SubscriptionTreeItemBase.contextValue, context);
-    await SubscriptionTreeItem.createChild(context as IManagedEnvironmentContext, subscription);
+export async function createManagedEnvironment(context: IActionContext, node?: AzExtParentTreeItem): Promise<void> {
+    if (!node) {
+        node = await ext.rgApi.appResourceTree.showTreeItemPicker<SubscriptionTreeItemBase>(SubscriptionTreeItemBase.contextValue, context);
+    }
+    await SubscriptionTreeItem.createChild(context as IManagedEnvironmentContext, node as SubscriptionTreeItemBase);
     await ext.rgApi.appResourceTree.refresh(context);
 }
