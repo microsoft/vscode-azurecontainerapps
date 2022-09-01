25abf9b326919f159a57ebfb4c27f026f5c96731
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2022-09-01 12:57:58 -0700

    Add Container App Delete Support to the Activity Log (#181)

---
+++ src/commands/deleteContainerApp/IDeleteContainerAppWizardContext.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ExecuteActivityContext, IActionContext, ISubscriptionContext } from "@microsoft/vscode-azext-utils";
+
+export interface IDeleteContainerAppWizardContext extends IActionContext, ExecuteActivityContext {
+    subscription: ISubscriptionContext;
+    resourceGroupName: string;
+    containerAppName: string;
+}
