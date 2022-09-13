35fea03395d7ccbbed9e5150b4a1cde3bef23725
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2022-09-13 10:09:06 -0700

    Added differentiating enabled/disabled Dapr tree items (#192)

--- src/tree/DaprTreeItem.ts
+++ src/tree/DaprTreeItem.ts
@@ -5,15 +5,16 @@
 
 import { Dapr } from "@azure/arm-appcontainers";
 import { AzExtParentTreeItem, AzExtTreeItem, GenericTreeItem, IActionContext, TreeItemIconPath } from "@microsoft/vscode-azext-utils";
+import { ThemeIcon } from "vscode";
 import { azResourceContextValue } from "../constants";
 import { localize } from "../utils/localize";
 import { treeUtils } from "../utils/treeUtils";
 import { ContainerAppTreeItem } from "./ContainerAppTreeItem";
 import { IAzureResourceTreeItem } from "./IAzureResourceTreeItem";
 
-export class DaprTreeItem extends AzExtParentTreeItem implements IAzureResourceTreeItem {
-    public static contextValue: string = 'dapr';
-    public readonly contextValue: string = `${DaprTreeItem.contextValue}|${azResourceContextValue}`;
+export class DaprEnabledTreeItem extends AzExtParentTreeItem implements IAzureResourceTreeItem {
+    public static contextValue: string = 'dapr|enabled';
+    public readonly contextValue: string = `${DaprEnabledTreeItem.contextValue}|${azResourceContextValue}`;
     public readonly parent: ContainerAppTreeItem;
     public data: Dapr;
 
@@ -23,7 +24,7 @@ export class DaprTreeItem extends AzExtParentTreeItem implements IAzureResourceT
         super(parent);
         this.label = localize('dapr', 'Dapr');
         this.data = data || {};
-        this.description = this.data.enabled ? 'Enabled' : 'Disabled';
+        this.description = localize('enabled', 'Enabled');
     }
 
     public get iconPath(): TreeItemIconPath {
@@ -32,14 +33,32 @@ export class DaprTreeItem extends AzExtParentTreeItem implements IAzureResourceT
 
     public async loadMoreChildrenImpl(_clearCache: boolean, _context: IActionContext): Promise<AzExtTreeItem[]> {
         const children: AzExtTreeItem[] = [];
-        this.data.appId ? children.push(new GenericTreeItem(this, { description: 'app id', label: this.data.appId, contextValue: 'daprAppId' })) : undefined;
-        this.data.appPort ? children.push(new GenericTreeItem(this, { description: 'app port', label: String(this.data.appPort), contextValue: 'daprAppPort' })) : undefined;
-        this.data.appProtocol ? children.push(new GenericTreeItem(this, { description: 'app protocol', label: String(this.data.appProtocol), contextValue: 'daprAppProtocol' })) : undefined;
+        this.data.appId ? children.push(new GenericTreeItem(this, { description: 'app id', label: this.data.appId, contextValue: 'daprAppId', iconPath: new ThemeIcon('dash') })) : undefined;
+        this.data.appPort ? children.push(new GenericTreeItem(this, { description: 'app port', label: String(this.data.appPort), contextValue: 'daprAppPort', iconPath: new ThemeIcon('dash') })) : undefined;
+        this.data.appProtocol ? children.push(new GenericTreeItem(this, { description: 'app protocol', label: String(this.data.appProtocol), contextValue: 'daprAppProtocol', iconPath: new ThemeIcon('dash') })) : undefined;
 
         return children;
     }
 
     public hasMoreChildrenImpl(): boolean {
         return false;
+    }
+}
+
+export class DaprDisabledTreeItem extends AzExtTreeItem {
+    public static contextValue: string = 'dapr|disabled';
+    public readonly contextValue: string = DaprDisabledTreeItem.contextValue;
+    public readonly parent: ContainerAppTreeItem;
+
+    public label: string;
+
+    constructor(parent: ContainerAppTreeItem) {
+        super(parent);
+        this.label = localize('dapr', 'Dapr');
+        this.description = localize('disabled', 'Disabled');
+    }
+
+    public get iconPath(): TreeItemIconPath {
+        return new ThemeIcon('debug-disconnect');
     }
 }
