954eb6a44afb2ab75bf23c15167a48ceffd919f6
Megan Mott <meganmott@microsoft.com>
2023-02-27 10:10:46 -0800

    Implement quick start image

---
+++ src/commands/createContainerApp/setQuickStartImage.ts
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.md in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import { IContainerAppContext } from "./IContainerAppContext";
+
+export function setQuickStartImage(context: IContainerAppContext): void {
+    context.image = 'mcr.microsoft.com/azuredocs/aci-helloworld';
+    context.enableIngress = true;
+    context.enableExternal = true;
+    context.targetPort = 80;
+}
