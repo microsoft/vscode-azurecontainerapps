c2be2a6de093965d3f6f94b4c832e9cf982a6aa2
Megan Mott <meganmott@microsoft.com>
2023-03-27 13:27:37 -0700

    Add logs

--- src/commands/imageSource/buildImageInAzure/BuildImageStep.ts
+++ src/commands/imageSource/buildImageInAzure/BuildImageStep.ts
@@ -8,6 +8,7 @@ import { acrDomain } from "../../../constants";
 import { localize } from "../../../utils/localize";
 import { IBuildImageInAzureContext } from "./IBuildImageInAzureContext";
 import { buildImageInAzure } from "./buildImageInAzure";
+import { getContentProvider } from "./openImageErrorLog";
 
 export class BuildImageStep extends AzureWizardExecuteStep<IBuildImageInAzureContext> {
     public priority: number = 225;
@@ -23,7 +24,10 @@ export class BuildImageStep extends AzureWizardExecuteStep<IBuildImageInAzureCon
             const image = outputImages[0];
             context.image = `${image.registry}/${image.repository}:${image.tag}`;
         } else {
-            throw new Error(localize('noImagesBuilt', 'Failed to build image.'));
+            const contentProvider = getContentProvider();
+            await contentProvider.openImageLog(context);
+            const errorMessage = localize('noImagesBuilt', 'Failed to build image. View logs for more details.');
+            throw new Error(errorMessage);
         }
     }
 
@@ -31,3 +35,4 @@ export class BuildImageStep extends AzureWizardExecuteStep<IBuildImageInAzureCon
         return !context.image;
     }
 }
+
---
+++ src/commands/imageSource/buildImageInAzure/openImageErrorLog.ts
@@ -0,0 +1,41 @@
+import { sendRequestWithTimeout } from "@microsoft/vscode-azext-azureutils";
+import { nonNullValue } from "@microsoft/vscode-azext-utils";
+import { TextDocumentContentProvider, Uri, window, workspace } from "vscode";
+import { ext } from "../../../extensionVariables";
+import { IBuildImageInAzureContext } from "./IBuildImageInAzureContext";
+
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.md in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+const contentScheme: string = 'image-error-log';
+
+let _cachedContentProvider: ImageLogContentProvider | undefined;
+export function getContentProvider(): ImageLogContentProvider {
+    if (!_cachedContentProvider) {
+        _cachedContentProvider = new ImageLogContentProvider();
+        ext.context.subscriptions.push(workspace.registerTextDocumentContentProvider(contentScheme, _cachedContentProvider));
+    }
+    return _cachedContentProvider;
+}
+
+class ImageLogContentProvider implements TextDocumentContentProvider {
+    private _contentMap: Map<string, string> = new Map<string, string>();
+
+    public async openImageLog(context: IBuildImageInAzureContext): Promise<void> {
+        const timeout = 4000;
+        const logSasUrl = (await context.client.runs.getLogSasUrl(context.resourceGroupName, context.registryName, nonNullValue(context.run.runId))).logLink;
+        const content = (await sendRequestWithTimeout(context, { method: 'GET', url: logSasUrl }, timeout, undefined)).bodyAsText;
+        const uri: Uri = Uri.parse(`${contentScheme}:///${context.imageName}.log`);
+         if (content) {
+            this._contentMap.set(uri.toString(), content);
+        }
+
+        await window.showTextDocument(uri);
+    }
+
+    public async provideTextDocumentContent(uri: Uri): Promise<string> {
+        const imageLogContent = nonNullValue(this._contentMap.get(uri.toString()));
+        return imageLogContent
+    }
+}
