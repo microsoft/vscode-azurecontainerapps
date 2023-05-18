d4b996fce41f8ef5d617809239e8305467848e65
MicroFish91 <mwfisher91@gmail.com>
2023-05-18 12:43:04 -0700

    toggleIngressVisibility

---
+++ src/commands/ingress/toggleIngressVisibility/toggleIngressVisibility.ts
@@ -0,0 +1,38 @@
+import { Ingress } from "@azure/arm-appcontainers";
+import { AzureWizard, AzureWizardExecuteStep, IActionContext, createSubscriptionContext, nonNullValueAndProp } from "@microsoft/vscode-azext-utils";
+import { IngressConstants } from "../../../constants";
+import { ContainerAppItem } from "../../../tree/ContainerAppItem";
+import { IngressItem } from "../../../tree/IngressItem";
+import { createActivityContext } from "../../../utils/activityUtils";
+import { localize } from "../../../utils/localize";
+import { pickContainerApp } from "../../../utils/pickContainerApp";
+import { IngressContext } from "../IngressContext";
+import { ToggleIngressVisibilityStep } from "./ToggleIngressVisibilityStep";
+
+export async function toggleIngressVisibility(context: IActionContext, node?: IngressItem | ContainerAppItem): Promise<void> {
+    const { subscription, containerApp } = node ?? await pickContainerApp(context);
+
+    const wizardContext: IngressContext = {
+        ...context,
+        ...createSubscriptionContext(subscription),
+        ...(await createActivityContext()),
+        subscription,
+        containerApp
+    };
+
+    const ingress: Ingress = nonNullValueAndProp(containerApp.configuration, 'ingress');
+    const title: string = localize('toggleIngressVisibility', 'Toggle ingress visibility to "{0}" for container app "{1}"', ingress.external ? IngressConstants.internal : IngressConstants.external, containerApp.name);
+
+    const executeSteps: AzureWizardExecuteStep<IngressContext>[] = [
+        new ToggleIngressVisibilityStep()
+    ];
+
+    const wizard: AzureWizard<IngressContext> = new AzureWizard(wizardContext, {
+        title,
+        executeSteps,
+        showLoadingPrompt: true
+    });
+
+    wizardContext.activityTitle = title;
+    await wizard.execute();
+}
