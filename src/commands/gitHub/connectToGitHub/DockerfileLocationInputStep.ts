e4510d991808c3b167534f8476c5604e821d5382
MicroFish91 <mwfisher91@gmail.com>
2023-05-19 15:32:59 -0700

    DockerfileStep impl

--- src/commands/gitHub/connectToGitHub/DockerfileLocationInputStep.ts
+++ src/commands/gitHub/connectToGitHub/DockerfileLocationInputStep.ts
@@ -3,27 +3,97 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { AzureWizardPromptStep } from "@microsoft/vscode-azext-utils";
+import { AzureWizardPromptStep, IAzureQuickPickItem, nonNullProp } from "@microsoft/vscode-azext-utils";
+import { GetRepoContentReqParams, RepoContent, getRepositoryContent } from "../../../gitHub/getRepositoryContent";
 import { localize } from "../../../utils/localize";
-import { validateUtils } from "../../../utils/validateUtils";
 import type { IConnectToGitHubContext } from "./IConnectToGitHubContext";
 
-export class DockerfileLocationInputStep extends AzureWizardPromptStep<IConnectToGitHubContext> {
-    public async prompt(context: IConnectToGitHubContext): Promise<void> {
-        context.dockerfilePath = (await context.ui.showInputBox({
-            value: './Dockerfile',
-            prompt: localize('dockerfileLocationPrompt', "Enter the relative location of the Dockerfile in the repository."),
-            validateInput: this.validateInput
-        })).trim();
+type ContentPickData = {
+    traverse?: 'Up' | 'Down',
+    contentName: string;
+}
 
+const dockerfile: string = 'Dockerfile';
+
+export class DockerfileLocationInputStep extends AzureWizardPromptStep<IConnectToGitHubContext> {
+    private cachedPicks: Map<string, IAzureQuickPickItem<ContentPickData>[]>;
+
+    public async prompt(context: IConnectToGitHubContext): Promise<void> {
+        // We always want fresh picks before prompting in case the user has pressed the back button
+        this.cachedPicks = new Map();
+
+        const placeHolder: string = localize('dockerfileLocationPrompt', "Select a 'Dockerfile' in the repository.");
+
+        let path: string = '';
+        while (true) {
+            const contentPickData = (await context.ui.showQuickPick(this.getPicks(context, path), { placeHolder })).data;
+
+            if (contentPickData.traverse === 'Up') {
+                path = path.split('/').slice(0, -1).join('/');
+            } else if (contentPickData.traverse === 'Down') {
+                path += '/' + contentPickData.contentName;
+            }
+
+            if (contentPickData.contentName === dockerfile) {
+                break;
+            }
+        }
+
+        context.dockerfilePath = path;
+        context.valuesToMask.push(path);
     }
 
     public shouldPrompt(context: IConnectToGitHubContext): boolean {
         return !context.dockerfilePath;
     }
 
-    private validateInput(dockerfilePath: string): string | undefined {
-        dockerfilePath = dockerfilePath ? dockerfilePath.trim() : '';
-        return !validateUtils.isValidLength(dockerfilePath) ? validateUtils.getInvalidLengthMessage() : undefined;
+    private async getPicks(context: IConnectToGitHubContext, path: string): Promise<IAzureQuickPickItem<ContentPickData>[]> {
+        let remotePicks: IAzureQuickPickItem<ContentPickData>[] = [];
+        if (!this.cachedPicks.has(path)) {
+            remotePicks = await this.getRemotePicks(context, path);
+            this.cachedPicks.set(path, remotePicks);
+        }
+
+        return this.cachedPicks.get(path) ?? remotePicks;
+    }
+
+    private async getRemotePicks(context: IConnectToGitHubContext, path: string): Promise<IAzureQuickPickItem<ContentPickData>[]> {
+        const repoContentParams: GetRepoContentReqParams = {
+            owner: context.gitHubOrg ?? context.gitHubRepositoryOwner ?? '',
+            repo: nonNullProp(context, 'gitHubRepository'),
+            path
+        };
+
+        let contents: RepoContent = await getRepositoryContent(context, repoContentParams);
+        if (!Array.isArray(contents)) {
+            contents = [contents];
+        }
+
+        const filteredContents = contents.filter((content) => content.type === 'dir' || content.name === dockerfile);
+        const picks: IAzureQuickPickItem<ContentPickData>[] = filteredContents.map((content) => {
+            const endsWith: string = content.type === 'dir' ? '/' : '';
+            const label: string = content.name + endsWith;
+            return {
+                label,
+                suppressPersistence: true,
+                data: { traverse: 'Down', contentName: content.name } };
+        });
+
+        const operationPicks: IAzureQuickPickItem<ContentPickData>[] = [
+            {
+                label: '.',
+                description: (path || '/') + '/',
+                suppressPersistence: true,
+                data: { traverse: undefined, contentName: '' }
+            },
+            {
+                label: '..',
+                description: path.split('/').slice(0, -1).join('/') + '/',
+                suppressPersistence: true,
+                data: { traverse: 'Up', contentName: '' }
+            }
+        ];
+
+        return path ? [...operationPicks, ...picks] : [...picks];
     }
 }
