f2c3f8147e0048e0b5e1db1c271aada701549476
MicroFish91 <mwfisher91@gmail.com>
2023-05-24 10:57:58 -0700

    Add createGraphClient

---
+++ src/utils/graphClient.ts
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { AuthenticationProvider, Client, ClientOptions } from "@microsoft/microsoft-graph-client";
+import "isomorphic-fetch"; // Requires importing this library or a fetch polyfill to use the graph client
+import { AuthenticationGetSessionOptions, AuthenticationSession, authentication } from "vscode";
+
+export async function createGraphClient(): Promise<Client> {
+    const scopes: string[] = ['https://graph.microsoft.com/.default'];
+    const sessionOptions: AuthenticationGetSessionOptions = {
+        clearSessionPreference: true,
+        createIfNone: true
+    };
+    const session: AuthenticationSession | undefined = await authentication.getSession('microsoft', scopes, sessionOptions);
+
+    if (!session) {
+        throw new Error('You are not signed in to a valid Microsoft Graph account. Please sign in and try again.');
+    }
+
+    const authProvider: AuthenticationProvider = {
+        // AuthenticationProvider expects 'getAccessToken' to return a Promise
+        getAccessToken: (): Promise<string> => {
+            return Promise.resolve(session.accessToken);
+        }
+    };
+    const clientOptions: ClientOptions = {
+        authProvider
+    };
+
+    return Client.initWithMiddleware(clientOptions);
+}
