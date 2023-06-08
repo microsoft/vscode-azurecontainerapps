21758106389d7e4dc669ed919a0bcf87e835a82c
MicroFish91 <mwfisher91@gmail.com>
2023-06-08 12:16:02 -0700

    Add localize

--- src/utils/graphClient.ts
+++ src/utils/graphClient.ts
@@ -6,6 +6,7 @@
 import { AuthenticationProvider, Client, ClientOptions } from "@microsoft/microsoft-graph-client";
 import "isomorphic-fetch"; // Requires importing this library or a fetch polyfill to use the graph client
 import { AuthenticationGetSessionOptions, AuthenticationSession, authentication } from "vscode";
+import { localize } from "./localize";
 
 export async function createGraphClient(): Promise<Client> {
     const scopes: string[] = ['https://graph.microsoft.com/.default'];
@@ -16,7 +17,7 @@ export async function createGraphClient(): Promise<Client> {
     const session: AuthenticationSession | undefined = await authentication.getSession('microsoft', scopes, sessionOptions);
 
     if (!session) {
-        throw new Error('You are not signed in to a valid Microsoft Graph account. Please sign in and try again.');
+        throw new Error(localize('notSignedIn', 'You are not signed in to a valid Microsoft Graph account. Please sign in and try again.'));
     }
 
     const authProvider: AuthenticationProvider = {
