eb526f9e513938068b4a2170281f6a3dbf11aade
MicroFish91 <mwfisher91@gmail.com>
2023-06-14 14:19:33 -0700

    Implementation

--- src/commands/revision/restartRevision.ts
+++ src/commands/revision/restartRevision.ts
@@ -4,10 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { IActionContext } from "@microsoft/vscode-azext-utils";
-import { ContainerAppItem } from "../../tree/ContainerAppItem";
-import { RevisionItem } from "../../tree/RevisionItem";
+import { RevisionItem } from "../../tree/revisionManagement/RevisionItem";
 import { executeRevisionOperation } from "./changeRevisionActiveState";
 
-export function restartRevision(context: IActionContext, node?: ContainerAppItem | RevisionItem): Promise<void> {
+export function restartRevision(context: IActionContext, node?: RevisionItem): Promise<void> {
     return executeRevisionOperation(context, node, 'restartRevision');
 }
--- src/tree/configurations/IngressItem.ts
+++ src/tree/configurations/IngressItem.ts
@@ -7,11 +7,11 @@ import { ContainerApp, Ingress } from "@azure/arm-appcontainers";
 import { createGenericElement } from "@microsoft/vscode-azext-utils";
 import { AzureSubscription, ViewPropertiesModel } from "@microsoft/vscode-azureresources-api";
 import { ThemeIcon, TreeItem, TreeItemCollapsibleState } from "vscode";
-import { IngressConstants, azResourceContextValue } from "../constants";
-import { localize } from "../utils/localize";
-import { treeUtils } from "../utils/treeUtils";
-import { ContainerAppModel } from "./ContainerAppItem";
-import { ContainerAppsItem, TreeElementBase } from "./ContainerAppsBranchDataProvider";
+import { IngressConstants, azResourceContextValue } from "../../constants";
+import { localize } from "../../utils/localize";
+import { treeUtils } from "../../utils/treeUtils";
+import { ContainerAppModel } from "../ContainerAppItem";
+import { ContainerAppsItem, TreeElementBase } from "../ContainerAppsBranchDataProvider";
 
 const label: string = localize('ingress', 'Ingress');
 
