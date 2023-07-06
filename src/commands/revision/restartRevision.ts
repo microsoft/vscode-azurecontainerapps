b4761039c310c0947a476cd693206601c905f8ed
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2023-07-06 11:37:05 -0700

    Remove `Latest` in single revision mode (#400)

--- src/commands/revision/restartRevision.ts
+++ src/commands/revision/restartRevision.ts
@@ -3,10 +3,11 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { IActionContext } from "@microsoft/vscode-azext-utils";
-import { RevisionItem } from "../../tree/revisionManagement/RevisionItem";
+import type { IActionContext } from "@microsoft/vscode-azext-utils";
+import type { ContainerAppItem } from "../../tree/ContainerAppItem";
+import type { RevisionItem } from "../../tree/revisionManagement/RevisionItem";
 import { executeRevisionOperation } from "./changeRevisionActiveState";
 
-export function restartRevision(context: IActionContext, node?: RevisionItem): Promise<void> {
+export function restartRevision(context: IActionContext, node?: ContainerAppItem | RevisionItem): Promise<void> {
     return executeRevisionOperation(context, node, 'restartRevision');
 }
