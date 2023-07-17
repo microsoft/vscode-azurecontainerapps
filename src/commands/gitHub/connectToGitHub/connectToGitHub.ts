9eca0cea524471492e8c0476a5fac3c16832c454
MicroFish91 <mwfisher91@gmail.com>
2023-07-17 12:45:02 -0700

    Impl

--- src/commands/gitHub/connectToGitHub/connectToGitHub.ts
+++ src/commands/gitHub/connectToGitHub/connectToGitHub.ts
@@ -3,22 +3,15 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { GitHubBranchListStep, GitHubOrgListStep, GitHubRepositoryListStep } from "@microsoft/vscode-azext-github";
-import { AzureWizard, AzureWizardExecuteStep, AzureWizardPromptStep, ITreeItemPickerContext, createSubscriptionContext } from "@microsoft/vscode-azext-utils";
-import type { ContainerAppItem } from "../../../tree/ContainerAppItem";
-import { createActivityContext } from "../../../utils/activityUtils";
+import { ITreeItemPickerContext, createSubscriptionContext, openUrl } from "@microsoft/vscode-azext-utils";
+import type { ContainerAppItem, ContainerAppModel } from "../../../tree/ContainerAppItem";
+import { createPortalUrl } from "../../../utils/createPortalUrl";
 import { localize } from "../../../utils/localize";
 import { pickContainerApp } from "../../../utils/pickContainerApp";
-import { AcrListStep } from "../../deployImage/imageSource/containerRegistry/acr/AcrListStep";
-import { AcrRepositoriesListStep } from "../../deployImage/imageSource/containerRegistry/acr/AcrRepositoriesListStep";
-import { DockerfileLocationInputStep } from "./DockerfileLocationInputStep";
-import { GitHubRepositoryConnectStep } from "./GitHubRepositoryConnectStep";
-import type { IConnectToGitHubContext } from "./IConnectToGitHubContext";
-import { ServicePrincipalIdInputStep } from "./ServicePrincipalIdInputStep";
-import { ServicePrincipalSecretInputStep } from "./ServicePrincipalSecretInputStep";
+import { IContainerAppContext } from "../../IContainerAppContext";
 import { isGitHubConnected } from "./isGitHubConnected";
 
-export async function connectToGitHub(context: ITreeItemPickerContext & Partial<IConnectToGitHubContext>, item?: Pick<ContainerAppItem, 'containerApp' | 'subscription'>): Promise<void> {
+export async function connectToGitHub(context: ITreeItemPickerContext, item?: Pick<ContainerAppItem, 'containerApp' | 'subscription'>): Promise<void> {
     if (!item) {
         context.suppressCreatePick = true;
         item = await pickContainerApp(context);
@@ -26,42 +19,22 @@ export async function connectToGitHub(context: ITreeItemPickerContext & Partial<
 
     const { subscription, containerApp } = item;
 
-    const wizardContext: IConnectToGitHubContext = {
+    // Make containerApp _required_
+    const containerAppContext: IContainerAppContext & { containerApp: ContainerAppModel } = {
         ...context,
         ...createSubscriptionContext(subscription),
-        ...await createActivityContext(),
         subscription,
-        containerApp,
+        containerApp
     };
 
-    if (await isGitHubConnected(wizardContext)) {
+    if (await isGitHubConnected(containerAppContext)) {
         throw new Error(localize('gitHubAlreadyConnected', '"{0}" is already connected to a GitHub repository.', containerApp.name));
     }
 
-    const title: string = localize('connectGitHubRepository', 'Connect a GitHub repository to "{0}"', containerApp.name);
-
-    const promptSteps: AzureWizardPromptStep<IConnectToGitHubContext>[] = [
-        new GitHubOrgListStep(),
-        new GitHubRepositoryListStep(),
-        new GitHubBranchListStep(),
-        new DockerfileLocationInputStep(),
-        new AcrListStep(),
-        new AcrRepositoriesListStep(),
-        new ServicePrincipalIdInputStep(),
-        new ServicePrincipalSecretInputStep()
-    ];
-
-    const executeSteps: AzureWizardExecuteStep<IConnectToGitHubContext>[] = [
-        new GitHubRepositoryConnectStep()
-    ];
-
-    const wizard: AzureWizard<IConnectToGitHubContext> = new AzureWizard(wizardContext, {
-        title,
-        promptSteps,
-        executeSteps,
-        showLoadingPrompt: true
-    });
-
-    await wizard.prompt();
-    await wizard.execute();
+    /**
+     * VS Code does not have sufficient scopes to handle CRUD on service principals & federated credentials.
+     * Bump users to the portal to avoid providing an incomplete/partial connection experience.
+     */
+    const portalUrl: string = createPortalUrl(subscription, `${containerApp.id}/continuousDeployment`).toString(true /** skipEncoding */);
+    await openUrl(portalUrl);
 }
