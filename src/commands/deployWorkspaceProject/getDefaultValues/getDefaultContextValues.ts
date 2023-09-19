e531cea04581185f7a58ec6c19f3116980600c1d
MicroFish91 <mwfisher91@gmail.com>
2023-09-19 15:07:08 -0700

    Impl

--- src/commands/deployWorkspaceProject/getDefaultValues/getDefaultContextValues.ts
+++ src/commands/deployWorkspaceProject/getDefaultValues/getDefaultContextValues.ts
@@ -4,25 +4,32 @@
 *--------------------------------------------------------------------------------------------*/
 
 import type { ISubscriptionActionContext } from "@microsoft/vscode-azext-utils";
+import { relativeSettingsFilePath } from "../../../constants";
+import { ext } from "../../../extensionVariables";
+import { localize } from "../../../utils/localize";
+import { EnvironmentVariablesListStep } from "../../deployImage/imageSource/EnvironmentVariablesListStep";
 import type { DeployWorkspaceProjectContext } from "../DeployWorkspaceProjectContext";
+import { DeployWorkspaceProjectSettings, getDeployWorkspaceProjectSettings } from "../DeployWorkspaceProjectSettings";
+import { getDefaultAcrResources } from "./getDefaultAcrResources";
+import { getDefaultContainerAppsResources } from "./getDefaultContainerAppsResources";
 import { getWorkspaceProjectPaths } from "./getWorkspaceProjectPaths";
 
 export async function getDefaultContextValues(context: ISubscriptionActionContext): Promise<Partial<DeployWorkspaceProjectContext>> {
     const { rootFolder, dockerfilePath } = await getWorkspaceProjectPaths(context);
 
-    // const settings: IDeployWorkspaceProjectSettings | undefined = await getDeployWorkspaceProjectSettings(rootFolder);
-    // if (!settings) {
-    //     ext.outputChannel.appendLog(localize('noWorkspaceSettings', 'Scanned and found no matching resource settings at "{0}".', relativeSettingsFilePath));
-    // } else if (!settings.containerAppResourceGroupName || !settings.containerAppName || !settings.containerRegistryName) {
-    //     ext.outputChannel.appendLog(localize('resourceSettingsIncomplete', 'Scanned and found incomplete container app resource settings at "{0}".', relativeSettingsFilePath));
-    // }
+    const settings: DeployWorkspaceProjectSettings | undefined = await getDeployWorkspaceProjectSettings(rootFolder);
+    if (!settings) {
+        ext.outputChannel.appendLog(localize('noWorkspaceSettings', 'Scanned and found no matching resource settings at "{0}".', relativeSettingsFilePath));
+    } else if (!settings.containerAppResourceGroupName || !settings.containerAppName || !settings.containerRegistryName) {
+        ext.outputChannel.appendLog(localize('resourceSettingsIncomplete', 'Scanned and found incomplete container app resource settings at "{0}".', relativeSettingsFilePath));
+    }
 
     return {
-        // ...await getDefaultContainerAppsResources(context, settings),
-        // ...await getDefaultAzureContainerRegistry(context, settings),
+        ...await getDefaultContainerAppsResources(context, settings),
+        ...await getDefaultAcrResources(context, settings),
         // newRegistrySku: KnownSkuName.Basic,
         dockerfilePath,
-        // environmentVariables: await EnvironmentVariablesListStep.workspaceHasEnvFile() ? undefined : [],
+        environmentVariables: await EnvironmentVariablesListStep.workspaceHasEnvFile() ? undefined : [],
         // imageSource: ImageSource.RemoteAcrBuild,
         // os: AcrBuildSupportedOS.Linux,
         rootFolder,
