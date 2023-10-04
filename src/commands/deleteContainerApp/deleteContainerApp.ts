54b5c9556c49af8b4b45182b05a3ae0cc5a84738
MicroFish91 <mwfisher91@gmail.com>
2023-10-03 21:29:24 -0700

    Impl

--- src/commands/deleteContainerApp/deleteContainerApp.ts
+++ src/commands/deleteContainerApp/deleteContainerApp.ts
@@ -5,12 +5,9 @@
 
 import { IActionContext } from "@microsoft/vscode-azext-utils";
 import { ContainerAppItem } from "../../tree/ContainerAppItem";
-import { localize } from "../../utils/localize";
 import { pickContainerApp } from "../../utils/pickItem/pickContainerApp";
 
 export async function deleteContainerApp(context: IActionContext, node?: ContainerAppItem): Promise<void> {
-    node ??= await pickContainerApp(context, {
-        title: localize('deleteContainerApp', 'Delete Container App'),
-    });
+    node ??= await pickContainerApp(context);
     await node.delete(context);
 }
