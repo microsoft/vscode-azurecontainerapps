333c662c3dd1ce2f9fd1fda4654550552f9b5704
MicroFish91 <mwfisher91@gmail.com>
2023-11-03 16:42:07 -0700

    Comment

--- src/utils/getVerifyProvidersStep.ts
+++ src/utils/getVerifyProvidersStep.ts
@@ -7,6 +7,9 @@ import { VerifyProvidersStep } from "@microsoft/vscode-azext-azureutils";
 import type { ISubscriptionActionContext } from "@microsoft/vscode-azext-utils";
 import { appProvider, operationalInsightsProvider, registryProvider, webProvider } from "../constants";
 
+/**
+ * Use to obtain a `VerifyProvidersStep` that registers all known container app providers to the user's subscription
+ */
 export function getVerifyProvidersStep<T extends ISubscriptionActionContext>(): VerifyProvidersStep<T> {
     return new VerifyProvidersStep<T>([
         appProvider,
