3f6da6c3a32c61dca043cffced23c0804fb77941
MicroFish91 <mwfisher91@gmail.com>
2023-11-03 21:18:41 -0700

    Impl

--- src/commands/image/imageSource/buildImageInAzure/ImageNameStep.ts
+++ src/commands/image/imageSource/buildImageInAzure/ImageNameStep.ts
@@ -6,22 +6,33 @@
 import { AzureWizardPromptStep } from "@microsoft/vscode-azext-utils";
 import { URI, Utils } from "vscode-uri";
 import { localize } from "../../../../utils/localize";
+import { validateUtils } from "../../../../utils/validateUtils";
 import { BuildImageInAzureImageSourceContext } from "./BuildImageInAzureContext";
 
 export class ImageNameStep extends AzureWizardPromptStep<BuildImageInAzureImageSourceContext> {
     public async prompt(context: BuildImageInAzureImageSourceContext): Promise<void> {
         const suggestedImageName = await getSuggestedName(context, context.rootFolder.name);
 
-        context.imageName = await context.ui.showInputBox({
+        context.imageName = (await context.ui.showInputBox({
             prompt: localize('imageNamePrompt', 'Enter a name for the image'),
-            value: suggestedImageName ? localize('dockerfilePlaceholder', suggestedImageName) : ''
-        });
+            value: suggestedImageName ? localize('dockerfilePlaceholder', suggestedImageName) : '',
+            validateInput: this.validateInput
+        })).trim();
     }
 
     public shouldPrompt(context: BuildImageInAzureImageSourceContext): boolean {
         return !context.imageName;
     }
 
+    private validateInput(name: string | undefined): string | undefined {
+        name = name ? name.trim() : '';
+
+        if (!validateUtils.isValidLength(name, 1, 46)) {
+            return validateUtils.getInvalidLengthMessage(1, 46);
+        }
+
+        return undefined;
+    }
 }
 
 async function getSuggestedName(context: BuildImageInAzureImageSourceContext, dockerFilePath: string): Promise<string | undefined> {
