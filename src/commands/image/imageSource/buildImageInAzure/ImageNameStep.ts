c2679804be305fc8ada1476a95acad04e13354a7
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2023-11-06 11:02:47 -0800

    Improve image name validation & suggestion (#560)

--- src/commands/image/imageSource/buildImageInAzure/ImageNameStep.ts
+++ src/commands/image/imageSource/buildImageInAzure/ImageNameStep.ts
@@ -6,22 +6,35 @@
 import { AzureWizardPromptStep } from "@microsoft/vscode-azext-utils";
 import { URI, Utils } from "vscode-uri";
 import { localize } from "../../../../utils/localize";
+import { validateUtils } from "../../../../utils/validateUtils";
 import { BuildImageInAzureImageSourceContext } from "./BuildImageInAzureContext";
+
+const maxImageNameLength: number = 46;
 
 export class ImageNameStep extends AzureWizardPromptStep<BuildImageInAzureImageSourceContext> {
     public async prompt(context: BuildImageInAzureImageSourceContext): Promise<void> {
         const suggestedImageName = await getSuggestedName(context, context.rootFolder.name);
 
-        context.imageName = await context.ui.showInputBox({
+        context.imageName = (await context.ui.showInputBox({
             prompt: localize('imageNamePrompt', 'Enter a name for the image'),
-            value: suggestedImageName ? localize('dockerfilePlaceholder', suggestedImageName) : ''
-        });
+            value: suggestedImageName ?? '',
+            validateInput: this.validateInput
+        })).trim();
     }
 
     public shouldPrompt(context: BuildImageInAzureImageSourceContext): boolean {
         return !context.imageName;
     }
 
+    private validateInput(name: string | undefined): string | undefined {
+        name = name ? name.trim() : '';
+
+        if (!validateUtils.isValidLength(name, 1, maxImageNameLength)) {
+            return validateUtils.getInvalidLengthMessage(1, maxImageNameLength);
+        }
+
+        return undefined;
+    }
 }
 
 async function getSuggestedName(context: BuildImageInAzureImageSourceContext, dockerFilePath: string): Promise<string | undefined> {
@@ -32,6 +45,9 @@ async function getSuggestedName(context: BuildImageInAzureImageSourceContext, do
             suggestedImageName = Utils.basename(context.rootFolder.uri).toLowerCase().replace(/\s/g, '');
         }
     }
-    suggestedImageName += ":{{.Run.ID}}";
+
+    const colonTag: string = ':latest';
+    suggestedImageName = suggestedImageName?.slice(0, maxImageNameLength - colonTag.length);
+    suggestedImageName += colonTag;
     return suggestedImageName;
 }
