74aa7e42ab08038f5fd87801b71beff6a2449b8e
MicroFish91 <mwfisher91@gmail.com>
2024-01-17 12:27:16 -0800

    Add path separators back

--- src/commands/revisionDraft/RevisionDraftFileSystem.ts
+++ src/commands/revisionDraft/RevisionDraftFileSystem.ts
@@ -188,9 +188,8 @@ export class RevisionDraftFileSystem implements FileSystemProvider {
 
     private buildUriFromItem(item: ContainerAppsItem): Uri {
         // Container app names are not globally unique, so we need to produce a unique ID for lookup
-        // Note: Using '|' delimiters instead of '/' to prevent each file as showing up within multiple virtual directories when opened
         const parsedResourceId: ParsedAzureResourceId = parseAzureResourceId(item.containerApp.id);
-        const shortenedContainerAppId: string = `${parsedResourceId.subscriptionId?.slice(0, 5)}...|resourceGroups|${parsedResourceId.resourceGroup}|containerApps|${parsedResourceId.resourceName}`;
+        const shortenedContainerAppId: string = `${parsedResourceId.subscriptionId?.slice(0, 5)}.../resourceGroups/${parsedResourceId.resourceGroup}/containerApps/${parsedResourceId.resourceName}`;
         return URI.parse(`${RevisionDraftFileSystem.scheme}:/${shortenedContainerAppId}.containerapp-template.json`);
     }
 
