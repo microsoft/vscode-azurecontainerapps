8948754066ba920fef55e6c9054aa474a30541a3
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2024-03-13 11:50:53 -0700

    Scaffold `getWorkspaceDeploymentConfiguration` and add updated `RootFolderStep` (#617)

--- src/commands/deployWorkspaceProject/deploymentConfiguration/workspace/getWorkspaceDeploymentConfiguration.ts
+++ src/commands/deployWorkspaceProject/deploymentConfiguration/workspace/getWorkspaceDeploymentConfiguration.ts
@@ -3,10 +3,31 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { AzureWizard, type AzureWizardExecuteStep, type AzureWizardPromptStep } from "@microsoft/vscode-azext-utils";
 import { type IContainerAppContext } from "../../../IContainerAppContext";
+import { RootFolderStep } from "../../../image/imageSource/buildImageInAzure/RootFolderStep";
 import { type DeploymentConfiguration } from "../DeploymentConfiguration";
+import { type WorkspaceDeploymentConfigurationContext } from "./WorkspaceDeploymentConfigurationContext";
 
 // Todo: Monorepo core logic (workspace settings path) https://github.com/microsoft/vscode-azurecontainerapps/issues/613
-export async function getWorkspaceDeploymentConfiguration(_: IContainerAppContext): Promise<DeploymentConfiguration> {
-    return {};
+export async function getWorkspaceDeploymentConfiguration(context: IContainerAppContext): Promise<DeploymentConfiguration> {
+    const wizardContext: WorkspaceDeploymentConfigurationContext = context;
+
+    const promptSteps: AzureWizardPromptStep<WorkspaceDeploymentConfigurationContext>[] = [
+        new RootFolderStep()
+    ];
+
+    const executeSteps: AzureWizardExecuteStep<WorkspaceDeploymentConfigurationContext>[] = [];
+
+    const wizard: AzureWizard<WorkspaceDeploymentConfigurationContext> = new AzureWizard(wizardContext, {
+        promptSteps,
+        executeSteps,
+    });
+
+    await wizard.prompt();
+    await wizard.execute();
+
+    return {
+        rootFolder: wizardContext.rootFolder,
+    };
 }
