63e9090c2c17d6ad0379645cb0bf641e568273cc
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-03-20 12:08:23 -0700

    Support emptying workspace

--- src/commands/walkthrough/addWorkspaceProject.ts
+++ src/commands/walkthrough/addWorkspaceProject.ts
@@ -3,8 +3,19 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { commands } from "vscode";
+import { type IActionContext } from "@microsoft/vscode-azext-utils";
+import { commands, workspace, type MessageItem } from "vscode";
+import { localize } from "../../utils/localize";
 
-export async function addWorkspaceProjectWalkthrough(): Promise<void> {
+export async function addWorkspaceProjectWalkthrough(context: IActionContext): Promise<void> {
+    if (workspace.workspaceFolders?.length) {
+        const warning: string = localize('workspaceWarning', 'Detected existing workspace content. You must start from an empty workspace.\n\nPress continue to reset your current workspace.');
+
+        const items: MessageItem[] = [{ title: localize('continue', 'Continue') }];
+        await context.ui.showWarningMessage(warning, { modal: true, stepName: 'addWorkspaceProject.emptyWorkspace' }, ...items);
+
+        workspace.updateWorkspaceFolders(0, workspace.workspaceFolders.length);
+    }
+
     await commands.executeCommand('git.clone');
 }
