b28dc3a8cecfbf898e07a695fa231fb5f009a55f
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-03-26 23:46:24 -0700

    Impl

--- src/commands/deployWorkspaceProject/deploymentConfiguration/workspace/DeploymentConfigurationListStep.ts
+++ src/commands/deployWorkspaceProject/deploymentConfiguration/workspace/DeploymentConfigurationListStep.ts
@@ -37,6 +37,9 @@ export class DeploymentConfigurationListStep extends AzureWizardPromptStep<Works
             return undefined;
         }
 
+        // We mainly want to show activity children if there are deployment settings to verify
+        context.activityChildren ??= [];
+
         return {
             executeSteps: [
                 new FilePathsVerifyStep(),
