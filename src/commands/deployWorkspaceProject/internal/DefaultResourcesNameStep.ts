0332022aa63950d010a4a7475d0fff7536006fc7
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-03-29 10:29:26 -0700

    Update log

--- src/commands/deployWorkspaceProject/internal/DefaultResourcesNameStep.ts
+++ src/commands/deployWorkspaceProject/internal/DefaultResourcesNameStep.ts
@@ -39,7 +39,7 @@ export class DefaultResourcesNameStep extends AzureWizardPromptStep<DeployWorksp
         const suffixNamesAvailable: boolean = await this.checkSuffixNamesAvailable(context, resourceName);
 
         if (suffixNamesAvailable) {
-            ext.outputChannel.appendLog(localize('suffixNamesAvailable', `Verified that the provided names with attached resource suffixes (e.g. ${resourceName + '-rg'}, ${resourceName + '-ca'}, etc.) are also available. Automatically attaching a suffix to each resource name.`));
+            ext.outputChannel.appendLog(localize('suffixNamesAvailable', `Verified that the provided names with attached resource suffixes (e.g. ${resourceName + '-rg'}, ${resourceName + '-ca'}, etc.) are also available. Automatically attaching suffixes to each resource name.`));
         } else {
             ext.outputChannel.appendLog(localize('suffixNamesUnavailable', `Unable to verify provided name with attached resource suffixes (e.g. ${resourceName + '-rg'}, ${resourceName + '-ca'}, etc.). Defaulting all resources to the original name provided.`));
         }
