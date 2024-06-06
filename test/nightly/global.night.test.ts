0925f60926d3f61b088bd47fefff93cb12bf63d4
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-06-06 13:58:17 -0700

    Update

--- test/nightly/global.night.test.ts
+++ test/nightly/global.night.test.ts
@@ -2,6 +2,9 @@ import { longRunningTestsEnabled } from "../global.test";
 
 suiteSetup(async function (this: Mocha.Context): Promise<void> {
     if (!longRunningTestsEnabled) {
-        this.skip();
+        console.debug("Skipping long running tests...");
+        return this.skip();
     }
+
+    console.debug('Running nightly tests...');
 });
