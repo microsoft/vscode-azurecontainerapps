9d9e4cd423ac64e16a526eaeb20f719b8f964f1f
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-06-06 15:38:40 -0700

    Add

--- test/nightly/nightly.test.ts
+++ test/nightly/nightly.test.ts
@@ -1,6 +1,13 @@
 import * as assert from 'assert';
+import { longRunningTestsEnabled } from '../global.test';
 
 suite('dummyTest', () => {
+    suiteSetup(async function (this: Mocha.Context): Promise<void> {
+        if (!longRunningTestsEnabled) {
+            this.skip();
+        }
+    });
+
     test('should not see', () => {
         assert.ok('should not run');
     })
