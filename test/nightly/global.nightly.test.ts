9831624c0cee02765cf03b0452bd2f3f72560444
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-06-13 12:46:07 -0700

    Add delete resource groups functionality

--- test/nightly/global.nightly.test.ts
+++ test/nightly/global.nightly.test.ts
@@ -3,7 +3,13 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { ResourceManagementClient } from '@azure/arm-resources';
+import { createAzureClient } from '@microsoft/vscode-azext-azureutils';
+import { createTestActionContext, type TestActionContext } from '@microsoft/vscode-azext-dev';
+import { createSubscriptionContext, subscriptionExperience, type ISubscriptionContext } from '@microsoft/vscode-azext-utils';
+import { type AzureSubscription } from '@microsoft/vscode-azureresources-api';
 import * as vscode from 'vscode';
+import { ext } from '../../extension.bundle';
 import { longRunningTestsEnabled } from '../global.test';
 
 export const resourceGroupsToDelete: string[] = [];
@@ -12,7 +18,32 @@ suiteSetup(async function (this: Mocha.Context): Promise<void> {
     if (!longRunningTestsEnabled) {
         return;
     }
+
     await vscode.commands.executeCommand('azureResourceGroups.logIn');
 });
 
-// Add suiteteardown for resourcegroups
+suiteTeardown(async function (this: Mocha.Context): Promise<void> {
+    if (!longRunningTestsEnabled) {
+        return;
+    }
+
+    await deleteResourceGroups();
+});
+
+// Todo: re-test this
+async function deleteResourceGroups(): Promise<void> {
+    const context: TestActionContext = await createTestActionContext();
+    const subscription: AzureSubscription = await subscriptionExperience(context, ext.rgApiV2.resources.azureResourceTreeDataProvider);
+    const subscriptionContext: ISubscriptionContext = createSubscriptionContext(subscription);
+
+    const rgClient: ResourceManagementClient = createAzureClient([context, subscriptionContext], ResourceManagementClient);
+    await Promise.all(resourceGroupsToDelete.map(async resourceGroup => {
+        if ((await rgClient.resourceGroups.checkExistence(resourceGroup)).body) {
+            console.log(`Started delete of resource group "${resourceGroup}"...`);
+            await rgClient.resourceGroups.beginDeleteAndWait(resourceGroup);
+            console.log(`Successfully started delete of resource group "${resourceGroup}".`);
+        } else {
+            console.log(`Ignoring resource group "${resourceGroup}" because it does not exist.`);
+        }
+    }));
+}
