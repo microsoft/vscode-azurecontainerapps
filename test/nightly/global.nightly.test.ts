13113509ea5ee8246b2f83ad77701a4d53b21d81
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-06-14 13:07:20 -0700

    Your guess is as good as mine

--- test/nightly/global.nightly.test.ts
+++ test/nightly/global.nightly.test.ts
@@ -4,9 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { ResourceManagementClient } from '@azure/arm-resources';
-import { createAzureClient } from '@microsoft/vscode-azext-azureutils';
+import { createAzureClient, registerAzureUtilsExtensionVariables } from '@microsoft/vscode-azext-azureutils';
 import { createTestActionContext, type TestActionContext } from '@microsoft/vscode-azext-dev';
-import { createSubscriptionContext, subscriptionExperience, type ISubscriptionContext } from '@microsoft/vscode-azext-utils';
+import { createSubscriptionContext, registerUIExtensionVariables, subscriptionExperience, type ISubscriptionContext } from '@microsoft/vscode-azext-utils';
 import { type AzureSubscription } from '@microsoft/vscode-azureresources-api';
 import * as vscode from 'vscode';
 import { ext } from '../../extension.bundle';
@@ -35,6 +35,10 @@ async function deleteResourceGroups(): Promise<void> {
     const context: TestActionContext = await createTestActionContext();
     const subscription: AzureSubscription = await subscriptionExperience(context, ext.rgApiV2.resources.azureResourceTreeDataProvider);
     const subscriptionContext: ISubscriptionContext = createSubscriptionContext(subscription);
+
+    registerUIExtensionVariables(ext);
+    registerAzureUtilsExtensionVariables(ext);
+
     const rgClient: ResourceManagementClient = createAzureClient([context, subscriptionContext], ResourceManagementClient);
 
     console.log("created resource group client")
