31570eaaba81d75998a6ae7b1e961e7dc8c64432
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-07-25 16:48:00 -0700

    Remove golang test case

--- test/nightly/deployWorkspaceProject/dwpTestScenarios.ts
+++ test/nightly/deployWorkspaceProject/dwpTestScenarios.ts
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { generateAlbumApiGolangTestCases } from "./testCases/albumApiGolangTestCases";
 import { generateAlbumApiJavaScriptTestCases } from "./testCases/albumApiJavaScriptTestCases";
 import { type DeployWorkspaceProjectTestCase } from "./testCases/DeployWorkspaceProjectTestCase";
 import { generateMonoRepoBasicTestCases } from "./testCases/monoRepoBasicTestCases";
@@ -15,11 +14,6 @@ interface DeployWorkspaceProjectTestScenario {
 }
 
 export const dwpTestScenarios: DeployWorkspaceProjectTestScenario[] = [
-    {
-        label: 'albumapi-go',
-        folderName: 'albumapi-go',
-        testCases: generateAlbumApiGolangTestCases()
-    },
     {
         label: 'albumapi-js',
         folderName: 'albumapi-js',
--- test/testProjects/containerapps-albumapi-go/README.md
+++
@@ -1,3 +0,0 @@
-# Azure Container Apps Album API
-
-Originally adapted from [this](https://github.com/azure-samples/containerapps-albumapi-go) repository, the companion repository for the [Azure Container Apps code-to-cloud quickstart](https://learn.microsoft.com/en-us/azure/container-apps/quickstart-code-to-cloud?tabs=bash%2Cgo&pivots=with-dockerfile).
