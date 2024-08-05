5caa1d897ab16513a653789fae54f88d119a3018
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-08-05 14:56:34 -0700

    Formatting

--- src/commands/registryCredentials/dockerLogin/listCredentialsFromAcr.ts
+++ src/commands/registryCredentials/dockerLogin/listCredentialsFromAcr.ts
@@ -9,9 +9,7 @@ import { nonNullProp, nonNullValue, nonNullValueAndProp, type ISubscriptionActio
 import { createContainerRegistryManagementClient } from "../../../utils/azureClients";
 import { type RegistryCredentialsContext } from "../RegistryCredentialsContext";
 
-export async function listCredentialsFromAcr(context: ISubscriptionActionContext & Partial<RegistryCredentialsContext>):
-    Promise<{ username: string, password: RegistryPassword }> {
-
+export async function listCredentialsFromAcr(context: ISubscriptionActionContext & Partial<RegistryCredentialsContext>): Promise<{ username: string, password: RegistryPassword }> {
     const containerClient: ContainerRegistryManagementClient = await createContainerRegistryManagementClient(context);
     const credentials = await containerClient.registries.listCredentials(getResourceGroupFromId(nonNullValueAndProp(context.registry, 'id')), nonNullValueAndProp(context.registry, 'name'));
     const password = credentials.passwords?.find(cred => cred.name === 'password' || cred.name === 'password2');
