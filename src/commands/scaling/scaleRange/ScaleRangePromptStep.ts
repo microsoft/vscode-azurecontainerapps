d8b48882cca0daa43276f090f086d603eed26c14
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2024-09-03 11:57:12 -0700

    Update scaling range validation to match the portal (#734)

--- src/commands/scaling/scaleRange/ScaleRangePromptStep.ts
+++ src/commands/scaling/scaleRange/ScaleRangePromptStep.ts
@@ -7,10 +7,12 @@ import { AzureWizardPromptStep } from '@microsoft/vscode-azext-utils';
 import { localize } from '../../../utils/localize';
 import { type ScaleRangeContext } from './ScaleRangeContext';
 
+const maxReplicas: number = 1000;
+
 export class ScaleRangePromptStep extends AzureWizardPromptStep<ScaleRangeContext> {
     public async prompt(context: ScaleRangeContext): Promise<void> {
         const scaleRange: string = (await context.ui.showInputBox({
-            prompt: localize('editScalingRange', 'Set the range of application replicas that get created in response to a scale rule. Set any range within the minimum of 0 and the maximum of 10 replicas'),
+            prompt: localize('editScalingRange', 'Set the range of application replicas that get created in response to a scale rule. Set any range within the minimum of 0 and the maximum of {0} replicas', maxReplicas),
             value: `${context.scaleMinRange}-${context.scaleMaxRange}`,
             validateInput: this.validateInput,
         })).trim();
@@ -25,14 +27,14 @@ export class ScaleRangePromptStep extends AzureWizardPromptStep<ScaleRangeContex
     }
 
     private validateInput(range: string | undefined): string | undefined {
-        const formatRegex = /^\d{1,2}-\d{1,2}$/;
+        const formatRegex = /^\d{1,3}-\d{1,4}$/;
         if (!range || !formatRegex.test(range)) {
-            return localize('enterRange', 'Please enter the range in the following format "0-10"');
+            return localize('enterRange', 'Please enter the range in the following format "0-{0}"', maxReplicas);
         }
 
         const [min, max] = range.split('-').map(range => Number(range));
-        if (min > 10 || max > 10) {
-            return localize('maxRangeExceeded', 'The maximum number of replicas is 10.');
+        if (min > maxReplicas || max > maxReplicas) {
+            return localize('maxRangeExceeded', 'The maximum number of replicas is {0}.', maxReplicas);
         } else if (min > max) {
             return localize('minHigherThanMax', 'The minimum range cannot be larger than the maximum range.');
         } else if (max === 0) {
