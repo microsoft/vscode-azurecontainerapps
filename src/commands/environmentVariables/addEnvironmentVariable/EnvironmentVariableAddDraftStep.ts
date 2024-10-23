10284d7fb58eae4a99bbc9cf5f86cbc3d6bfbce4
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-10-23 13:13:04 -0700

    Add environment variable logic

---
+++ src/commands/environmentVariables/addEnvironmentVariable/EnvironmentVariableAddDraftStep.ts
@@ -0,0 +1,39 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { type Container } from "@azure/arm-appcontainers";
+import { type Progress } from "vscode";
+import { type ContainerAppItem } from "../../../tree/ContainerAppItem";
+import { type RevisionsItemModel } from "../../../tree/revisionManagement/RevisionItem";
+import { localize } from "../../../utils/localize";
+import { RevisionDraftUpdateBaseStep } from "../../revisionDraft/RevisionDraftUpdateBaseStep";
+import { type EnvironmentVariableAddContext } from "./EnvironmentVariableAddContext";
+
+export class EnvironmentVariableAddDraftStep<T extends EnvironmentVariableAddContext> extends RevisionDraftUpdateBaseStep<T> {
+    public priority: number = 590;
+
+    constructor(baseItem: ContainerAppItem | RevisionsItemModel) {
+        super(baseItem);
+    }
+
+    public async execute(context: T, progress: Progress<{ message?: string | undefined; increment?: number | undefined }>): Promise<void> {
+        progress.report({ message: localize('addingEnv', 'Adding environment variable (draft)...') });
+        this.revisionDraftTemplate.containers ??= [];
+
+        const container: Container = this.revisionDraftTemplate.containers[context.containersIdx] ?? {};
+        container.env ??= [];
+        container.env.push({
+            name: context.newEnvironmentVariableName,
+            value: context.newEnvironmentVariableManualInput ?? '', // The server doesn't accept this value to be undefined
+            secretRef: context.secretName,
+        });
+
+        await this.updateRevisionDraftWithTemplate(context);
+    }
+
+    public shouldExecute(context: T): boolean {
+        return context.containersIdx !== undefined && !!context.newEnvironmentVariableName;
+    }
+}
---
+++ src/commands/environmentVariables/addEnvironmentVariable/EnvironmentVariableTypeListStep.ts
@@ -0,0 +1,54 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { AzureWizardPromptStep, type IAzureQuickPickItem, type IWizardOptions } from "@microsoft/vscode-azext-utils";
+import { localize } from "../../../utils/localize";
+import { SecretListStep } from "../../secret/SecretListStep";
+import { type EnvironmentVariableAddContext } from "./EnvironmentVariableAddContext";
+import { EnvironmentVariableManualInputStep } from "./EnvironmentVariableManualInputStep";
+
+export enum EnvironmentVariableType {
+    ManualInput = 'manual',
+    SecretRef = 'secretRef',
+}
+
+export class EnvironmentVariableTypeListStep<T extends EnvironmentVariableAddContext> extends AzureWizardPromptStep<T> {
+    public async prompt(context: T): Promise<void> {
+        const placeHolder: string = localize('environmentVariableTypePrompt', 'Select an environment variable type');
+        const picks: IAzureQuickPickItem<EnvironmentVariableType>[] = [
+            {
+                label: localize('manualLabel', 'Manual entry'),
+                data: EnvironmentVariableType.ManualInput,
+                suppressPersistence: true
+            },
+            {
+                label: localize('secretRefLabel', 'Reference a secret'),
+                data: EnvironmentVariableType.SecretRef,
+                suppressPersistence: true
+            },
+        ];
+        context.newEnvironmentVariableType = (await context.ui.showQuickPick(picks, { placeHolder })).data;
+    }
+
+    public shouldPrompt(context: T): boolean {
+        return !context.newEnvironmentVariableType;
+    }
+
+    public async getSubWizard(context: T): Promise<IWizardOptions<T> | undefined> {
+        const promptSteps: AzureWizardPromptStep<T>[] = [];
+
+        switch (context.newEnvironmentVariableType) {
+            case EnvironmentVariableType.ManualInput:
+                promptSteps.push(new EnvironmentVariableManualInputStep());
+                break;
+            case EnvironmentVariableType.SecretRef:
+                promptSteps.push(new SecretListStep({ suppressCreatePick: true }));
+                break;
+            default:
+        }
+
+        return { promptSteps };
+    }
+}
