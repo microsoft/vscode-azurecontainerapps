dd49395c05efb2a828f3533b098a42cb533222f6
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-12-03 10:17:32 -0800

    editEnvironmentVariableName implementation

---
+++ src/commands/environmentVariables/editEnvironmentVariable/EnvironmentVariableEditContext.ts
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.md in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import { type EnvironmentVar } from "@azure/arm-appcontainers";
+import { type SetTelemetryProps } from "../../../telemetry/SetTelemetryProps";
+import { type ContainerUpdateTelemetryProps as TelemetryProps } from "../../../telemetry/commandTelemetryProps";
+import { type ISecretContext } from "../../secret/ISecretContext";
+import { type EnvironmentVariablesBaseContext } from "../EnvironmentVariablesContext";
+import { type EnvironmentVariableType } from "../addEnvironmentVariable/EnvironmentVariableTypeListStep";
+
+export interface EnvironmentVariableEditBaseContext extends EnvironmentVariablesBaseContext, Pick<ISecretContext, 'secretName'> {
+    newEnvironmentVariableType?: EnvironmentVariableType;
+    newEnvironmentVariableName?: string;
+    newEnvironmentVariableManualInput?: string;
+    // secretName
+
+    environmentVariable: EnvironmentVar;
+}
+
+export type EnvironmentVariableEditContext = EnvironmentVariableEditBaseContext & SetTelemetryProps<TelemetryProps>;
