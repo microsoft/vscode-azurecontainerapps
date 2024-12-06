d8a71964a8098ae4900e591469fbb43ad1bb9cb8
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2024-12-06 13:51:26 -0800

    Add an ingress prompt when more than the image name tag changes (#800)

---
+++ test/imageNameUtils.test.ts
@@ -0,0 +1,61 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as assert from "assert";
+import { getImageNameWithoutTag } from "../extension.bundle";
+
+suite('imageNameUtils', () => {
+    test('getImageNameWithoutTag', () => {
+        const testValues: string[] = [
+            '',
+            'myimage:latest',
+            'repository/name:1.0.0',
+            'custom-registry.com/myimage:v2',
+            'library/ubuntu:20.04',
+            'project/service-name:dev-build',
+            'docker.io/library/nginx:stable',
+            'my-repo/my-service:release-candidate',
+            'anotherrepo/anotherimage:test',
+            'image_without_tag',
+            'my-image:with:multiple:colons',
+            'some-registry.io/nested/repo/image:12345',
+            'edgecase-without-tag:',
+            'dockerhub.com/image-name:alpha-beta',
+            'registry.example.com:5000/repo/image:tagname',
+            'private-repo/myimage:',
+            'test-image-with-special-characters:beta@123',
+            'path/to/image:no-colon-in-name',
+            'simple-image:another:tag',
+            'example.com:8080/repo/image:v3'
+        ];
+
+        const expectedValues: string[] = [
+            '',
+            'myimage',
+            'repository/name',
+            'custom-registry.com/myimage',
+            'library/ubuntu',
+            'project/service-name',
+            'docker.io/library/nginx',
+            'my-repo/my-service',
+            'anotherrepo/anotherimage',
+            'image_without_tag',
+            'my-image:with:multiple',
+            'some-registry.io/nested/repo/image',
+            'edgecase-without-tag',
+            'dockerhub.com/image-name',
+            'registry.example.com:5000/repo/image',
+            'private-repo/myimage',
+            'test-image-with-special-characters',
+            'path/to/image',
+            'simple-image:another',
+            'example.com:8080/repo/image'
+        ];
+
+        for (let i = 0; i < testValues.length; i++) {
+            assert.equal(getImageNameWithoutTag(testValues[i]), expectedValues[i]);
+        }
+    });
+});
