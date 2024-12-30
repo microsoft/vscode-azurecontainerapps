d3c884c599b5f855f4f18473b4853804187e3b46
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-12-30 13:49:33 -0800

    Make command consistent with other deploy commands

--- src/commands/deployImage/deployImage.ts
+++ src/commands/deployImage/deployImage.ts
@@ -3,9 +3,13 @@
 *  Licensed under the MIT License. See License.md in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import { AzureWizard, createSubscriptionContext, nonNullValueAndProp, type IActionContext, type ISubscriptionContext } from "@microsoft/vscode-azext-utils";
+import { type ResourceGroup } from "@azure/arm-resources";
+import { LocationListStep, ResourceGroupListStep } from "@microsoft/vscode-azext-azureutils";
+import { activityInfoIcon, activitySuccessContext, AzureWizard, createSubscriptionContext, createUniversallyUniqueContextValue, GenericTreeItem, nonNullValue, nonNullValueAndProp, type IActionContext, type ISubscriptionContext } from "@microsoft/vscode-azext-utils";
+import { ext } from "../../extensionVariables";
 import { type ContainerItem } from "../../tree/containers/ContainerItem";
 import { createActivityContext } from "../../utils/activityUtils";
+import { isAzdExtensionInstalled } from "../../utils/azdUtils";
 import { getManagedEnvironmentFromContainerApp } from "../../utils/getResourceUtils";
 import { getVerifyProvidersStep } from "../../utils/getVerifyProvidersStep";
 import { localize } from "../../utils/localize";
@@ -32,7 +36,37 @@ export async function deployImage(context: IActionContext & Partial<ContainerReg
         template: nonNullValueAndProp(getParentResource(containerApp, node.revision), 'template'),
     };
 
-    wizardContext.telemetry.properties.revisionMode = containerApp.revisionsMode;
+    if (isAzdExtensionInstalled()) {
+        wizardContext.telemetry.properties.isAzdExtensionInstalled = 'true';
+    }
+
+    const resourceGroups: ResourceGroup[] = await ResourceGroupListStep.getResourceGroups(wizardContext);
+    wizardContext.resourceGroup = nonNullValue(
+        resourceGroups.find(rg => rg.name === containerApp.resourceGroup),
+        localize('containerAppResourceGroup', 'Expected to find the container app\'s resource group.'),
+    );
+
+    // Log resource group
+    wizardContext.activityChildren?.push(
+        new GenericTreeItem(undefined, {
+            contextValue: createUniversallyUniqueContextValue(['useExistingResourceGroupInfoItem', activitySuccessContext]),
+            label: localize('useResourceGroup', 'Using resource group "{0}"', wizardContext.resourceGroup.name),
+            iconPath: activityInfoIcon
+        })
+    );
+    ext.outputChannel.appendLog(localize('usingResourceGroup', 'Using resource group "{0}".', wizardContext.resourceGroup.name));
+
+    // Log container app
+    wizardContext.activityChildren?.push(
+        new GenericTreeItem(undefined, {
+            contextValue: createUniversallyUniqueContextValue(['useExistingContainerAppInfoItem', activitySuccessContext]),
+            label: localize('useContainerApp', 'Using container app "{0}"', wizardContext.containerApp?.name),
+            iconPath: activityInfoIcon
+        })
+    );
+    ext.outputChannel.appendLog(localize('usingContainerApp', 'Using container app "{0}".', wizardContext.containerApp?.name));
+
+    await LocationListStep.setLocation(wizardContext, containerApp.location);
 
     const parentResourceName: string = getParentResource(containerApp, node.revision).name ?? containerApp.name;
     const wizard: AzureWizard<DeployImageContext> = new AzureWizard(wizardContext, {
