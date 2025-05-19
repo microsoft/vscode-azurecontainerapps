6735c42000d05a6c63f956253e22fffe5c63e548
Megan Mott <meganmott@microsoft.com>
2025-05-19 09:49:03 -0700

    initial commit

---
+++ src/webviews/ConfirmationView.tsx
@@ -0,0 +1,153 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Button, createTableColumn, DataGrid, DataGridBody, DataGridCell, DataGridRow, TableCellLayout, Tooltip, type OnSelectionChangeData, type SelectionItemId, type TableColumnDefinition } from '@fluentui/react-components';
+import { Sparkle16Filled } from '@fluentui/react-icons';
+import * as React from 'react';
+import { useContext, useState, type JSX } from 'react';
+import './confirmationView.scss';
+import { type ConfirmationViewControllerType } from './confirmationViewController';
+import { useConfiguration } from './webview-client/useConfiguration';
+import { WebviewContext } from './WebviewContext';
+
+type Item = {
+    name: string;
+    value: string;
+}
+
+
+
+function createNewSetWithItem(set: Set<SelectionItemId>, first: boolean): Set<SelectionItemId> {
+    if (first) {
+        const firstElement = Array.from(set)[0];
+        return new Set([firstElement]);
+    } else {
+        const lastElement = Array.from(set)[set.size - 1];
+        return new Set([lastElement]);
+    }
+}
+
+export const ConfirmationView = (): JSX.Element => {
+    const [title, setTitle] = useState('Confirm');
+    const [selectedItems, setSelectedItems] = useState<Set<SelectionItemId>>(new Set());
+    const { vscodeApi } = useContext(WebviewContext);
+
+    const configuration = useConfiguration<ConfirmationViewControllerType>();
+    const confirmClicked = () => {
+        const selectedItemIndex = Number(Array.from(selectedItems)[0]);
+        const itemsToClear = configuration.length - selectedItemIndex;
+        vscodeApi.postMessage({
+            command: 'confirm',
+            itemsToClear: itemsToClear
+        });
+        console.log(itemsToClear);
+    };
+
+    const copilotClicked = (name: string, value: string) => {
+        vscodeApi.postMessage({
+            command: 'copilot',
+            itemsToClear: 0,
+            name: name,
+            value: value
+        });
+    }
+
+    const cancelClicked = () => {
+        vscodeApi.postMessage({
+            command: 'cancel'
+        })
+    };
+
+    const onChange = (_ev: React.MouseEvent<HTMLInputElement, MouseEvent>, data: OnSelectionChangeData) => {
+        setTitle(data.selectedItems.size > 0 ? 'Edit' : 'Confirm');
+        setSelectedItems(data.selectedItems.size > 0 ? createNewSetWithItem(data.selectedItems, false) : createNewSetWithItem(data.selectedItems, true));
+    }
+
+    const columnSizingOptions = {
+        name: {
+            defaultWidth: 200,
+            minWidth: 200,
+        },
+        value: {
+            defaultWidth: 200,
+            minWidth: 200,
+        }
+    }
+
+    const columns: TableColumnDefinition<Item>[] = [
+        createTableColumn<Item>({
+            columnId: 'name',
+            renderCell: (item: Item) => {
+                return <TableCellLayout >{item.name}</TableCellLayout >;
+            },
+        }),
+        createTableColumn<Item>({
+            columnId: 'value',
+            renderCell: (item: Item) => {
+                return <TableCellLayout className='value'> {item.value}
+                    <Tooltip content='Ask Copilot' relationship='label'>
+                        <Button
+                            appearance='transparent' icon={<Sparkle16Filled />} onClick={() => { void copilotClicked(item.name, item.value); }}>
+                        </Button>
+                    </Tooltip>
+                </TableCellLayout >;
+            },
+        })
+    ]
+
+    const DataGridComponent: React.FC<{
+        context: ConfirmationViewControllerType,
+        selectedItems?: Set<SelectionItemId> | undefined,
+        onSelectionChange: (_ev: React.MouseEvent<HTMLInputElement, MouseEvent>, data: OnSelectionChangeData) => void,
+    }> = ({ context, selectedItems, onSelectionChange },) => {
+        return (
+            <DataGrid
+                items={context}
+                columns={columns}
+                onSelectionChange={onSelectionChange}
+                selectionMode='multiselect'
+                selectedItems={selectedItems}
+                columnSizingOptions={columnSizingOptions}
+                resizableColumns
+                resizableColumnsOptions={{
+                    autoFitColumns: false,
+                }}>
+                <DataGridBody<Item>>
+                    {({ item, rowId }) => (
+                        <DataGridRow<Item>
+                            key={rowId}>
+                            {({ renderCell }) => (
+                                <DataGridCell>{renderCell(item)}</DataGridCell>
+                            )}
+                        </DataGridRow>
+                    )}
+                </DataGridBody>
+            </DataGrid >
+        );
+    };
+
+    return (
+        <div className='confirmationView'>
+            <div className='header'>
+                <h1>
+                    Confirm + Deploy
+                </h1>
+                <div>Please select any inputs you would like to change. Otherwise click "Confirm" to deploy.</div>
+            </div>
+
+            <div className='viewContent'>
+                <DataGridComponent context={configuration} selectedItems={selectedItems} onSelectionChange={onChange} />
+                <div className='buttonsView'>
+                    <Button appearance='primary' onClick={confirmClicked} size='large' >
+                        {title}
+                    </Button>
+                    <Button appearance='secondary' onClick={cancelClicked} size='large' >
+                        Cancel
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ webpack.config.views.js
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+/* eslint-env node */
+
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, { mode }) => {
+    const isDev = mode === 'development';
+
+    return {
+        // stats: 'detailed',
+        target: 'web',
+        mode: mode || 'none',
+        entry: {
+            views: './src/webviews/index.tsx',
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js',
+            libraryTarget: 'module',
+        },
+        experiments: {
+            outputModule: true,
+        },
+        resolve: {
+            roots: [__dirname],
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(tsx?)?$/iu,
+                    use: {
+                        loader: 'swc-loader',
+                        options: {
+                            module: {
+                                type: 'es6',
+                            },
+                            isModule: true,
+                            sourceMaps: isDev,
+                            jsc: {
+                                keepClassNames: true,
+                                target: 'es2018',
+                                parser: {
+                                    syntax: 'typescript',
+                                    tsx: true,
+                                },
+                            },
+                        },
+                    },
+                    exclude: /node_modules/u,
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        'style-loader',
+                        // Translates CSS into CommonJS
+                        'css-loader',
+                        // Compiles Sass to CSS
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.ttf$/,
+                    type: 'asset/resource',
+                },
+            ],
+        },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'src/webviews/static'),
+                publicPath: '/static',
+            },
+            allowedHosts: 'all',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+                'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+            },
+            hot: true,
+            host: '127.0.0.1',
+            client: {
+                overlay: true,
+            },
+            compress: true,
+            port: 8080,
+            webSocketServer: 'ws',
+        },
+        plugins: [
+            new webpack.ProvidePlugin({ React: 'react' }),
+            isDev && new webpack.HotModuleReplacementPlugin(),
+            new CopyWebpackPlugin({
+                patterns: [{ from: 'src/webviews/static', to: 'static', noErrorOnMissing: true }].filter(Boolean),
+            }),
+            new webpack.optimize.LimitChunkCountPlugin({
+                maxChunks: 1,
+            }),
+        ],
+        devtool: isDev ? 'source-map' : false,
+        infrastructureLogging: {
+            level: 'log', // enables logging required for problem matchers
+        },
+    };
+};
