846529726b1a9b03f8447e26a2aa3783bc905170
Megan Mott <meganmott@microsoft.com>
2025-07-01 16:03:51 -0700

    small change

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { gulp_webpack } from '@microsoft/vscode-azext-dev';
+import * as child_process from 'child_process';
 import * as fse from 'fs-extra';
 import * as gulp from 'gulp';
 import * as path from 'path';
@@ -26,6 +27,13 @@ async function cleanReadme(): Promise<void> {
     await fse.writeFile(readmePath, data);
 }
 
+async function installSwcCore(): Promise<void> {
+    if (process.platform === 'linux') {
+        child_process.execSync('npm install @swc/core-linux-x64-gnue --save-dev')
+    }
+}
+
 exports['webpack-dev'] = gulp.series(prepareForWebpack, () => gulp_webpack('development'));
 exports['webpack-prod'] = gulp.series(prepareForWebpack, () => gulp_webpack('production'));
 exports.cleanReadme = cleanReadme;
+exports.installSwcCore = installSwcCore;
