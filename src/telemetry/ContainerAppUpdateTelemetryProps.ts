0e14be3738d85a58293defa282a3296aa58e4ea4
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-07-18 12:54:18 -0700

    Telemetry should be optional

--- src/telemetry/ContainerAppUpdateTelemetryProps.ts
+++ src/telemetry/ContainerAppUpdateTelemetryProps.ts
@@ -4,5 +4,5 @@
 *--------------------------------------------------------------------------------------------*/
 
 export interface ContainerAppUpdateTelemetryProps {
-    addedContainerAppUpdateVerifyLogs: 'true' | 'false';
+    addedContainerAppUpdateVerifyLogs?: 'true' | 'false';
 }
