be564651a5c6736582ee5a8f9f0edeafff9c5046
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-07-20 20:41:27 -0700

    Update telemetry comment

--- src/telemetry/ContainerAppStartVerificationTelemetryProps.ts
+++ src/telemetry/ContainerAppStartVerificationTelemetryProps.ts
@@ -7,9 +7,9 @@ export interface ContainerAppStartVerificationTelemetryProps {
     /**
      * Indicates the result of attempting to add logs during container app start verification.
      *
-     * undefined — Did not attempt to add logs.
-     * 'false'   — Attempted to add logs but failed.
-     * 'true'    — Successfully added logs.
+     * 1. `undefined` — Did not attempt to add logs.
+     * 2. `false`     — Attempted to add logs but failed.
+     * 3. `true`      — Successfully added logs.
      */
     addedContainerAppStartLogs?: 'true' | 'false';
     getLogsQueryError?: string;
