563a58a7c7cb1934a4807ea6e25e19997fa173e8
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-09-01 15:01:30 +0900

    Rethrow better error message

--- src/commands/walkthrough/addWorkspaceProject.ts
+++ src/commands/walkthrough/addWorkspaceProject.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { UserCancelledError, type IActionContext } from "@microsoft/vscode-azext-utils";
+import { parseError, UserCancelledError, type IActionContext, type IParsedError } from "@microsoft/vscode-azext-utils";
 import { commands, workspace, type MessageItem } from "vscode";
 import { localize } from "../../utils/localize";
 
@@ -18,5 +18,13 @@ export async function addWorkspaceProjectWalkthrough(context: IActionContext): P
         throw new UserCancelledError();
     }
 
-    await commands.executeCommand('git.clone');
+    try {
+        await commands.executeCommand('git.clone');
+    } catch (e) {
+        const perr: IParsedError = parseError(e);
+        if (/git\.clone.*not found/i.test(perr.message)) {
+            throw new Error(localize('gitCloneNotFound', 'Command "git.clone" not found. This could be due to one of the following reasons:\n\n- Git is not installed on your system.\n- Git is installed but not added to your system PATH.\n- Git is installed but is broken or permission-blocked.\n\nPlease ensure Git is installed and properly configured.'));
+        }
+        throw e;
+    }
 }
