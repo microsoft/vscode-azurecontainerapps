8a7d298e63f4346b62ecfb9af3213ba2e94b8aff
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2025-09-02 20:39:08 +0900

    Delete duplicated project files

--- test/testProjects/advanced-js/src/controllers/AlbumController.js
+++
@@ -1,7 +0,0 @@
-var Albums = require("../models/Album");
-
-exports.index = async function (req, res) {
-  const albums = await Albums.getAlbums();
-  console.log("Retrieved Albums");
-  res.json(albums);
-};
--- test/testProjects/monorepo-identity/README.md
+++
@@ -1,3 +0,0 @@
-# Monorepo-basic
-
-This extremely minimal Node.js monorepo is used for quickly testing simple scenarios in the Azure Container Apps Extension.
