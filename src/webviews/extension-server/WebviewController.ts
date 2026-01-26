e79107cedc0360036d5d409b05284d92442d3bee
Megan Mott <meganmott@microsoft.com>
2026-01-26 09:20:20 -0800

    fix lint errors

--- src/webviews/extension-server/WebviewController.ts
+++ src/webviews/extension-server/WebviewController.ts
@@ -2,6 +2,7 @@
 *  Copyright (c) Microsoft Corporation. All rights reserved.
 *  Licensed under the MIT License. See License.md in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
+
 import { nonNullValue } from '@microsoft/vscode-azext-utils';
 import * as vscode from 'vscode';
 import { SharedState } from '../OpenConfirmationViewStep';
--- src/webviews/index.tsx
+++ src/webviews/index.tsx
@@ -2,6 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
 import './styles/global.scss';
 
 import * as React from 'react';
